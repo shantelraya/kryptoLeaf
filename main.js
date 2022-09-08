a7b322e88c4517eede67c9d41c6198d5e0873c13
shantelraya <parischax@gmail.com>
2022-09-08 18:36:45 +0300

    new commit

---
+++ main.js
@@ -0,0 +1,83 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>
+{
+menu.classList.toggle('fa-times');
+navbar.classList.toggle('active');
+}
+
+var swiper = new swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+    },
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    loop:true,
+});
+
+var swiper = new swiper(".anime-slider", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+    },
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    loop:true
+});
+
+
+var swiper = new swiper(".action-slider", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+    },
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    loop:true
+});
+
+var swiper = new swiper(".kids-slider", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+    },
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    loop:true
+});
+
+var swiper = new swiper(".family-slider", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+    },
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    loop:true
+});
