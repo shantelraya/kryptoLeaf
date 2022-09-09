b426d3419d09621466ecf66c32b7c9eebe9dbf68
shantelraya <parischax@gmail.com>
2022-09-09 10:51:18 +0300

    new js commit

--- main.js
+++ main.js
@@ -1,83 +1,10 @@
-let menu = document.querySelector('#menu-bars');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () =>
-{
-menu.classList.toggle('fa-times');
-navbar.classList.toggle('active');
+const MenuBtn = document.getElementById("Menu-Btn")
+const OtherNavbar = document.getElementById("Other-Navbar")
+MenuBtn.onclick = ()=>{
+    if (OtherNavbar.classList.contains("show")) {
+        OtherNavbar.classList.remove("show")
+    }
+    else{
+        OtherNavbar.classList.add("show")
+    }
 }
-
-var swiper = new swiper(".home-slider", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-    delay: 7500,
-    disableOnInteraction: false,
-    },
-    pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    },
-    loop:true,
-});
-
-var swiper = new swiper(".anime-slider", {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-    delay: 4500,
-    disableOnInteraction: false,
-    },
-    pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    },
-    loop:true
-});
-
-
-var swiper = new swiper(".action-slider", {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-    delay: 3500,
-    disableOnInteraction: false,
-    },
-    pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    },
-    loop:true
-});
-
-var swiper = new swiper(".kids-slider", {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-    },
-    pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    },
-    loop:true
-});
-
-var swiper = new swiper(".family-slider", {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-    delay: 1500,
-    disableOnInteraction: false,
-    },
-    pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    },
-    loop:true
-});
