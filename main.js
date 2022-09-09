2b0640da9fe85e386ded536630cf0f0068b1b6ae
shantelraya <parischax@gmail.com>
2022-09-09 10:40:56 +0300

    new commit

--- main.js
+++ main.js
@@ -1,84 +1,83 @@
-
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
+menu.onclick = () =>
+{
+menu.classList.toggle('fa-times');
+navbar.classList.toggle('active');
 }
 
 var swiper = new swiper(".home-slider", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 7500,
-      disableOnInteraction: false,
+    delay: 7500,
+    disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    el: ".swiper-pagination",
+    clickable: true,
     },
     loop:true,
-  });
+});
 
-  var swiper = new swiper(".anime-slider", {
+var swiper = new swiper(".anime-slider", {
     slidesPerView: 4,
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 4500,
-      disableOnInteraction: false,
+    delay: 4500,
+    disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    el: ".swiper-pagination",
+    clickable: true,
     },
     loop:true
-  });
+});
 
 
-  var swiper = new swiper(".action-slider", {
+var swiper = new swiper(".action-slider", {
     slidesPerView: 4,
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 3500,
-      disableOnInteraction: false,
+    delay: 3500,
+    disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    el: ".swiper-pagination",
+    clickable: true,
     },
     loop:true
-  });
+});
 
-  
-  var swiper = new swiper(".kids-slider", {
+var swiper = new swiper(".kids-slider", {
     slidesPerView: 4,
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
+    delay: 2500,
+    disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    el: ".swiper-pagination",
+    clickable: true,
     },
     loop:true
-  });
+});
 
-  var swiper = new swiper(".family-slider", {
+var swiper = new swiper(".family-slider", {
     slidesPerView: 4,
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 1500,
-      disableOnInteraction: false,
+    delay: 1500,
+    disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    el: ".swiper-pagination",
+    clickable: true,
     },
     loop:true
-  });
+});
